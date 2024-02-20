227e69ea4974b2c9b2fb491fb0e0f4cd3529aa90
AbhiSmith <abhishekv4004@gmail.com>
2024-02-20 21:45:09 +0530

    initial commite

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Header from "@/components/shared/Header"
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from 'next/navigation';
+  
+  const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
 
-const AddTranformationTypePage = () => {
+    const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId);
+
   return (
-    <div>AddTranformationTypePage</div>
+    <>
+
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+
+          <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+   
+   </>
   )
 }
 
-export default AddTranformationTypePage
+export default AddTransformationTypePage;
